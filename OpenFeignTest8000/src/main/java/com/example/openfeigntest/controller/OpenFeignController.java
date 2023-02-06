adf2cd976ffe96580e2959e45df3c1704b62e6c1
neo2195666 <115769198+neo2195666@users.noreply.github.com>
2023-02-07 02:27:14 +0800

    Update OpenFeignController.java

--- OpenFeignTest8000/src/main/java/com/example/openfeigntest/controller/OpenFeignController.java
+++ OpenFeignTest8000/src/main/java/com/example/openfeigntest/controller/OpenFeignController.java
@@ -11,17 +11,17 @@ public class OpenFeignController {
     @Autowired
     private OpenFeignClient openFeignClient;
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",produces = {"text/html;charset=UTF-8"})
     public String getNoParam(){
         return openFeignClient.method1();
     }
 
-    @RequestMapping("/getwithparams")
+    @RequestMapping(value = "/getwithparams",produces = {"text/html;charset=UTF-8"})
     public String getWithParams(String name,int age){
         return openFeignClient.getWithParams(name,age);
     }
 
-    @RequestMapping("/restful/{name}/{id}")
+    @RequestMapping(value = "/restful/{name}/{id}",produces = {"text/html;charset=UTF-8"})
     public String restful(@PathVariable("name") String name, @PathVariable("id") int id){
         return openFeignClient.restful(name,id);
     }
